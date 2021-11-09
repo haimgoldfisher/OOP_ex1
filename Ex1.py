7ff3667826f4cd9638d0436072df305c8c819d67
Or Yitshak <tur3103@gmail.com>
2021-11-09 21:15:12 +0200

    fixed json reading in exeporter

--- Ex1.py
+++ Ex1.py
@@ -9,23 +9,22 @@ def Ex1 (Building:str, Calls:str, Output:str): # <Building.json> <Calls.csv> <ou
     # we want list of elevators
 
 
-
-
-
-
-
-def exporter(Building, Calls, output):
+def exporter(building, calls, output):
         try:
-            with open(Building, "w") as my_building:
-                json.dumps(str, fp=my_building)  # building JSON importing
+            with open(building, "r") as my_building:
+                my_d = json.load(my_building)  # building JSON reading
         except ImportError as err:
             print(err)
         try:
-            my_calls = pd.read_csv(Calls)  # calls csv importing
+            my_calls = pd.read_csv(calls)  # calls csv importing
         except ImportError as err:
             print(err)
         try:
             my_output = pd.read_csv(output)  # output csv importing
         except ImportError as err:
             print(err)
-        return (my_building, my_calls, my_output)
+        return (my_d, my_calls, my_output)
+d1, d2, d3 = exporter("data/Ex1_input/Ex1_Buildings/B1.json","data/Ex1_input/Ex1_Calls/Calls_a.csv","data/Ex1_input/Ex1_Calls/Calls_a.csv")
+print(d1)
+print(d2)
+print(d3)
