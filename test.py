9f7881d4a99b7976fc7273f182845e10a51a4a39
HaimGoldfisher <haimngoldfisher@gmail.com>
2021-11-14 22:01:01 +0200

    test file adding

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from Elevator import Elevator
+from Ex1 import Ex1
+
+class Testing(unittest.TestCase):
+    def test_Elevator(self):
+        elev = Elevator()
+
+    #def test_Ex1(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
