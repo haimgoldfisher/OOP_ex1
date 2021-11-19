7a09b6f5f1c0401eaa48b6e2e45e077cc0a3cc86
HaimGoldfisher <haimngoldfisher@gmail.com>
2021-11-19 10:50:10 +0200

    testing

--- test.py
+++ test.py
@@ -2,10 +2,31 @@ import unittest
 
 from Elevator import Elevator
 from Ex1 import Ex1
+import json
 
 class Testing(unittest.TestCase):
     def test_Elevator(self):
-        elev = Elevator()
+        building = \
+        {
+            "_minFloor": -2,
+            "_maxFloor": 10,
+            "_elevators": [
+                {
+                    "_id": 0,
+                    "_speed": 0.5,
+                    "_minFloor": -2,
+                    "_maxFloor": 10,
+                    "_closeTime": 2.0,
+                    "_openTime": 2.0,
+                    "_startTime": 3.0,
+                    "_stopTime": 3.0
+                }
+            ]
+        }
+        json_dmb = json.dumps(building)
+        my_elev = json.loads(json_dmb)
+        assert (type(my_elev) is dict)
+
 
     #def test_Ex1(self):
 
