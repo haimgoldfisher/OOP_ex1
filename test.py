9a7ed0f18f15a2a998900001d680bb12b24932b6
HaimGoldfisher <haimngoldfisher@gmail.com>
2021-11-19 16:07:14 +0200

    test update - good luck

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from Elevator import Elevator
-from Ex1 import Ex1, time_calc, elevator_allocation
+from Ex1 import Ex1, time_calc, elevator_allocation, exporter
 import json
 import pandas as pd
 
@@ -33,7 +33,13 @@ class Testing(unittest.TestCase):
         elev_dct = Elevator("1", 1, 2, 3, 4, 5, 6, 7)
         self.assertNotEqual(elev_dct["_speed"], elev_dct["_stopTime"])
 
-#def test_Ex1(self):
+    def test_Ex1(self):
+        building, calls, output = exporter("B1.json", "Calls_a.csv", "Calls_a.csv")
+        for i in range(len(calls)):
+            if calls[i]["Source"] > calls[i]["Destination"]:
+                self.assertTrue(calls[i]["Direction"])
+            else:
+                self.assertFalse(calls[i]["Direction"])
 
 
 if __name__ == '__main__':
